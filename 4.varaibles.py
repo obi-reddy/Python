af0bee7b5925050ba35f0344e37cbcdb32d08445
obi-reddy <93593081+obi-reddy@users.noreply.github.com>
2021-12-12 09:10:24 +0530

    Add files via upload

---
+++ 4.varaibles.py
@@ -0,0 +1,15 @@
+"""variables and assigning values"""
+
+a=72
+print("The value of a is")
+print(a)
+
+
+""" assign a value to another variable"""
+
+b=a
+
+print(b)
+
+print("The value of a and b are")
+print(a,b)
