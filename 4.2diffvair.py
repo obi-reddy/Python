86e8724b16c0308f0b00e8f849834d912793251a
obi-reddy <93593081+obi-reddy@users.noreply.github.com>
2021-12-12 09:15:49 +0530

    Add files via upload

---
+++ 4.2diffvair.py
@@ -0,0 +1,17 @@
+""" Declaration of variables"""
+
+number=72
+
+_number=73
+
+N_umber=74
+
+NUMBER=75
+
+NUMBER76=76
+
+num77er=77
+
+print("The values of different declaration of variables")
+
+print(number,_number,N_umber,NUMBER,NUMBER76,num77er)
