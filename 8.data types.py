1dcc100e4dfd9dccf054557824c59a52db461041
KANCHI REDDY GARI OBI REDDY <93593081+obi-reddy@users.noreply.github.com>
2021-12-13 06:54:24 +0530

    Add files via upload

---
+++ 8.data types.py
@@ -0,0 +1,23 @@
+""" few  Data types"""
+
+""" int data type"""
+a=1
+print(a)
+
+""" float data type"""
+
+b=1.0
+print(b)
+
+""" String data type """
+
+d="This is a string data type """
+print(d)
+
+""" complex data type"""
+
+e=1+2j
+
+print(e)
+
+
