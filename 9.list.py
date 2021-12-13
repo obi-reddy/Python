40b8b037820b773ad43e8b2e33b740f6c5525cec
KANCHI REDDY GARI OBI REDDY <93593081+obi-reddy@users.noreply.github.com>
2021-12-13 07:05:07 +0530

    Add files via upload

---
+++ 9.list.py
@@ -0,0 +1,30 @@
+""" Example on list data types"""
+
+list = [1,2,3,"A","B"]
+
+print(list)
+
+
+""" Slicing """
+
+print(list[0:3])
+
+
+""" again slicing """
+
+print(list[2:])
+
+""" List repetation using * operator"""
+
+print(list*9)
+
+
+""" again slicing """
+
+print(list[2:4])
+
+
+
+""" again slicing """
+
+print(list[:-2])
