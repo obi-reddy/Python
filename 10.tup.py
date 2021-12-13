f18a8d67c301759f9237a2cfa3c1561742af6b5d
KANCHI REDDY GARI OBI REDDY <93593081+obi-reddy@users.noreply.github.com>
2021-12-13 07:42:52 +0530

    Add files via upload

---
+++ 10.tup.py
@@ -0,0 +1,13 @@
+""" Example on tuple data type"""
+
+tup=(1,2,3,"A","B","C")
+
+print(tup)
+
+""" Tuple slicing """
+
+print(tup[-5:2])
+
+
+""" Tuple addition using same  data type and same variable"""
+print(tup+tup)
